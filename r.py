b10c9396f356db1c2c45471646387ff2acd1169c
Art Yeh <e5a27123@gmail.com>
2020-05-16 15:22:30 +0800

    avg_len

--- r.py
+++ r.py
@@ -1,9 +1,13 @@
 data = []
 count = 0
+datalen = 0
 with open('reviews.txt', 'r') as f:
     for line in f:
         data.append(line)
+        datalen = len(data[count]) + datalen
         count += 1
         if count % 1000 == 0:
             print(len(data))
 print(data[0])
+datalen = datalen / len(data)
+print('平均是', datalen)
