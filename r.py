ea5e54b73d9b773867b38e61629b4d7f1f2a4636
Art Yeh <e5a27123@gmail.com>
2020-06-02 15:40:33 +0800

    add progressbar

--- r.py
+++ r.py
@@ -1,18 +1,21 @@
+import time
+import progressbar
+
 data = []
 count = 0
-datalen = 0
+
+bar = progressbar.ProgressBar(max_value=1000000)
 with open('reviews.txt', 'r') as f:
     for line in f:
         data.append(line)
-        datalen = len(data[count]) + datalen
         count += 1
-        if count % 1000 == 0:
-            print(len(data))
-print(data[0])
+        bar.update(count)
+print('檔案讀取完成,總共有', len(data), '筆資料')
 
-
-datalen = datalen / len(data)
-print('留言平均長度是', datalen)
+datalen = 0
+for d in data:
+    datalen = datalen + len(d)
+print('留言平均長度是', datalen/len(data))
 
 new = []
 for d in data:
@@ -28,6 +31,7 @@ good =[d for d in data if 'good' in d]
 print('一共有', len(good), '比留言提到good')
 
 #文字記數
+start_time = time.time()
 wc = {} #word_count
 for d in data:
     words = d.split(' ')
@@ -39,6 +43,9 @@ for d in data:
 for word in wc:
     if wc[word] > 10000000:
         print(word, wc[word])
+end_time = time.time()
+print('花費', end_time - start_time, 'seconds')
+
 print(len(wc))
 
 while True:
